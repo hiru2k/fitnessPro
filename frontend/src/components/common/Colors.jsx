abeb31ce05aea687bf2b24e8161da7f8347dd67a
hiru2k <mudannayakehiruni90@gmail.com>
2024-04-24 02:49:09 +0530

    chnaged color codes

--- frontend/src/components/common/Colors.jsx
+++ frontend/src/components/common/Colors.jsx
@@ -1,6 +1,7 @@
 export const Colors = {
-  primary: "#0085fe", //orange
-  secondary: "#38393D", //background dark black
-  gray: "#4A4A4A", // light black
-  white: "#FCFBFF", //white
+  primary: "#0085fe", //highlight blue
+  secondary: "#1b0c0d", //background dark black
+  darkBlue: "#124072",
+  gray: "#c3c3c3",
+  white: "#FCFBFF",
 };
